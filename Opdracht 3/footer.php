49683e3277d46a331674947c53df7f7dbae5ac8f
motasm20 <99595997+motasm20@users.noreply.github.com>
2024-06-18 11:37:35 +0200

    Delete Opdracht 3 directory

--- Opdracht 3/footer.php
+++
@@ -1,4 +0,0 @@
-<footer>
-    <p class="footerText">Made with love</p>
-    <p class="date"><?= date('d-m-y h:i:s') ?></p>
-</footer>
--- Opdracht 3/header.php
+++
@@ -1,4 +0,0 @@
-<header>
-    <img class="logo" src="img/logo.png"/>
-    <h1 class="title">PHP opdracht 3</h1>
-</header>
